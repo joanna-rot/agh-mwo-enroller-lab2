e9c4d0441a0c3bcb8bdabf8174f98c5fdc82535c
student66 <jzajacszymura@gmail.com>
2023-04-15 16:03:09 +0200

    security2

--- src/main/java/com/company/enroller/Security/WebSecurity.java
+++ src/main/java/com/company/enroller/Security/WebSecurity.java
@@ -1,5 +1,6 @@
 package com.company.enroller.security;
 
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -8,11 +9,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests()
-                .anyRequest().permitAll()
-                .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(participantProvider).passwordEncoder(passwordEncoder);
     }
-}
+    //protected void configure(HttpSecurity http) throws Exception {
+    //    http.csrf().disable()
+    //            .authorizeRequests()
+    //            .anyRequest().permitAll()
+    //            .and()
+    //            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
